1383165e12384a96484b87a4390f0bfb22acf7a5
SebasGil-Dev <SebasGil-Dev@users.noreply.github.com>
2026-04-16 23:51:31 -0500

    Agregar decoradores de validacion al DTO de bloques de horario

--- Back/src/modules/student-portal/application/dto/upsert-student-schedule-block.dto.ts
+++ Back/src/modules/student-portal/application/dto/upsert-student-schedule-block.dto.ts
@@ -1,10 +1,48 @@
+import {
+  IsDateString,
+  IsIn,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  Matches,
+  ValidateIf,
+} from 'class-validator';
+
 export class UpsertStudentScheduleBlockDto {
+  @IsIn(['ESPECIFICO', 'RECURRENTE'])
   type!: 'ESPECIFICO' | 'RECURRENTE';
+
+  @ValidateIf((o) => o.type === 'ESPECIFICO')
+  @IsDateString()
+  @IsNotEmpty()
   specificDate!: string | null;
+
+  @ValidateIf((o) => o.type === 'RECURRENTE')
+  @IsString()
+  @IsNotEmpty()
   dayOfWeek!: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @Matches(/^\d{2}:\d{2}$/, { message: 'startTime debe tener formato HH:MM' })
   startTime!: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @Matches(/^\d{2}:\d{2}$/, { message: 'endTime debe tener formato HH:MM' })
   endTime!: string;
+
+  @ValidateIf((o) => o.type === 'RECURRENTE')
+  @IsDateString()
+  @IsNotEmpty()
   recurrenceStartDate!: string;
+
+  @ValidateIf((o) => o.type === 'RECURRENTE')
+  @IsDateString()
+  @IsNotEmpty()
   recurrenceEndDate!: string;
-  reason!: string;
+
+  @IsOptional()
+  @IsString()
+  reason?: string;
 }
